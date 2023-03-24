cc8b168c96561c6c45483458b78021fe8d5c3c78
manquintana <manuelalejandroquintana@gmail.com>
2023-03-24 12:18:15 +0100

    Added multiple forms, persistence and edit_order feature with order id after persistence

--- nandiasgarden-project/pizza/forms.py
+++ nandiasgarden-project/pizza/forms.py
@@ -1,6 +1,22 @@
 from django import forms
+from .models import Pizza, Size
 
-class PizzaForm(forms.Form):
-    topping1 = forms.CharField(label='Topping 1', max_length=100)
-    topping2 = forms.CharField(label='Topping 2', max_length=100)
-    size = forms.ChoiceField(label='Size', choices=[('Small','Small'), ('Medium','Medium'), ('Large','Large')])
+# class PizzaForm(forms.Form):
+#     topping1 = forms.CharField(label='Topping 1', max_length=100)
+#     topping2 = forms.CharField(label='Topping 2', max_length=100)
+#     size = forms.ChoiceField(label='Size', choices=[('Small','Small'), ('Medium','Medium'), ('Large','Large')])
+''' Ejemplo de uso de widgets'''
+    # toppings = forms.MultipleChoiceField(choices=[('pep','Pepperoni'), ('cheese', 'Cheese'), ('olives', 'Olives')], widget=forms.CheckboxSelectMultiple) 
+    # size = forms.ChoiceField(label='Size', choices=[('Small','Small'), ('Medium','Medium'), ('Large','Large')])
+
+
+class PizzaForm(forms.ModelForm):
+    # image = forms.ImageField() # Agrega un boton "Adjuntar imagen" en el form
+    class Meta:
+        model = Pizza
+        fields = ['topping_1', 'topping_2', 'size']
+        labels = {'topping_1': 'Select Topping 1', 'topping_': 'Select Topping 2'}
+        # widgets = {'size':forms.CheckboxSelectMultiple}
+
+class MultiplePizzaForm(forms.Form):
+    number = forms.IntegerField(min_value=2, max_value=6)
--- nandiasgarden-project/pizza/models.py
+++ nandiasgarden-project/pizza/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Size(models.Model):
+    title = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
+    
+class Pizza(models.Model):
+    topping_1 = models.CharField(max_length=100)
+    topping_2 = models.CharField(max_length=100)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+
